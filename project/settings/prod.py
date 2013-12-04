816b2b117c4e856bc5a4116de9b311b69ca4db87
Morgan W <morganfwilliams@gmail.com>
2013-12-04 10:36:43 -0700

    Adding host

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -3,4 +3,4 @@ from base import *
 DEBUG = False
 
 # This should be set to the production domain (e.g. django-angular-pt.com)
-ALLOWED_HOSTS = ['http://sippies.herokuapp.com/']
+ALLOWED_HOSTS = ['sippies.herokuapp.com']
